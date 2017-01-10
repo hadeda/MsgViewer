12f200da126d136802162d3f3364e708f3d3043d
hangstrap <hangstrap@gmail.com>
2017-01-11 08:15:52 +1300

    add an example unit test

---
+++ src/test/java/za/co/digix/ExampleTest.java
@@ -0,0 +1,12 @@
+package za.co.digix;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void example(){
+        Assert.assertTrue( true);
+    }
+}
